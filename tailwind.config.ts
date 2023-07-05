5a1c7f0543094b32a58b58c778ad201d599f1b54
utkarsh575 <utkarshlohani575@gmail.com>
2023-07-05 17:11:39 +0530

    :wrench: added colors to tw config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,15 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#292D61",
+        secondary: "#AEB7FF",
+        active: "#41466A",
+        tertiary: "#17192F",
+        dark: "#0F0F0F",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
