07c7b6fa50cbbbe40ec752d3e446cc1c504ee0e4
utkarsh575 <utkarshlohani575@gmail.com>
2023-07-06 18:00:27 +0530

    :memo: added data to cardData.js

--- src/components/Layout/AppContent.tsx
+++ src/components/Layout/AppContent.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import Navbar from "../Navbar";
-import Card from "../ui/Card"
+import Card from "../ui/Card";
 import InfoCard from "../ui/Card/InfoCard";
 import Placeholder from "../ui/Card/Placeholder";
+import {
+  basicData,
+  returnMetricsData,
+  riskMetricsData,
+} from "../../services/cardData";
 const pages = [
   "Overview",
   "Portfolio",
@@ -19,26 +24,60 @@ export default function AppContent() {
     <section className=" mx-5 flex h-[95%] w-full flex-col overflow-scroll xl:overflow-x-hidden ">
       <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
       <section className=" h-full ">
-      <Placeholder heading="Basics" >
-        <Card title="1,206,915.097 USDC" primaryText="$1,206,915.097" secondaryText="aslkdhnfkjlads" imageUrl="/wallet.svg"/>
-        <Card title="1,206,915.097 USDC" primaryText="$1,206,915.097" secondaryText="aslkdhnfkjlads" imageUrl="/wallet.svg"/>
-        <Card title="1,206,915.097 USDC" primaryText="$1,206,915.097" secondaryText="Share Supply" imageUrl="/wallet.svg"/>
-        <Card title="1,206,915.097 USDC" primaryText="$1,206,915.097" secondaryText="aslkdhnfkjlads" imageUrl="/wallet.svg"/>
-      </Placeholder>
-      <Placeholder heading="Return Metrics">
-        <InfoCard metric="Return Month-to-Date" value="-7.05%" />
-        <InfoCard metric="Return Month-to-Date" value="-7.05%" />
-        <InfoCard metric="Return Month-to-Date" value="-7.05%" />
-        <InfoCard metric="Return Month-to-Date" value="-7.05%" />
-        <InfoCard metric="Return Month-to-Date" value="-7.05%" />
-        <InfoCard metric="Return Month-to-Date" value="-7.05%" />
-        <InfoCard metric="Return Month-to-Date" value="-7.05%" />
-        <InfoCard metric="Return Month-to-Date" value="-7.05%" />
-      </Placeholder>
-      <Placeholder heading="Risk Metrics">
-        <InfoCard metric="Annualised Volatility" value="88.86%" className="w-[47%]"/>
-        <InfoCard metric="Shape Ratio" value="0.47" className="w-[47%]"/>
-      </Placeholder>
+        {basicData.map((e) => {
+          return (
+            <>
+              <Placeholder heading={e.heading}>
+                {e.info.map((e) => {
+                  return (
+                    <>
+                      <Card
+                        title={e.title}
+                        primaryText={e.primaryText}
+                        secondaryText={e.secondaryText}
+                        imageUrl={e.imageUrl}
+                      />
+                    </>
+                  );
+                })}
+              </Placeholder>
+            </>
+          );
+        })}
+        {returnMetricsData.map((e) => {
+          return (
+            <>
+              <Placeholder heading={e.heading}>
+                {e.info.map((e) => {
+                  return (
+                    <>
+                      <InfoCard metric={e.metric} value={e.value} />
+                    </>
+                  );
+                })}
+              </Placeholder>
+            </>
+          );
+        })}
+        {riskMetricsData.map((e) => {
+          return (
+            <>
+              <Placeholder heading={e.heading}>
+                {e.info.map((e) => {
+                  return (
+                    <>
+                      <InfoCard
+                        metric={e.metric}
+                        value={e.value}
+                        className={e.className}
+                      />
+                    </>
+                  );
+                })}
+              </Placeholder>
+            </>
+          );
+        })}
       </section>
     </section>
   );
---
+++ src/services/cardData.js
@@ -0,0 +1,85 @@
+export const basicData = [
+  {
+    heading: "Basics",
+    info: [
+      {
+        title: "1,206,915.097 USDC",
+        primaryText: "$1,206,915.097",
+        secondaryText: "Gross Asset Value (GAV)",
+        imageUrl: "/wallet.svg",
+      },
+      {
+        title: "1,206,915.097 USDC",
+        primaryText: "$1,206,915.097",
+        secondaryText: "Net Asset Value (NAV)",
+        imageUrl: "/wallet.svg",
+      },
+      {
+        title: "1,206,915.097 USDC",
+        primaryText: "$1,206,915.097",
+        secondaryText: "Share Supply",
+        imageUrl: "/wallet.svg",
+      },
+      {
+        title: "1,206,915.097 USDC",
+        primaryText: "$1,206,915.097",
+        secondaryText: "Share Price",
+        imageUrl: "/wallet.svg",
+      },
+    ],
+  },
+];
+
+export const returnMetricsData = [
+  {heading: "Return Metrics",
+  info: [
+    {
+      metric: "Return Month-to-Date",
+      value: "-7.05%",
+    },
+    {
+      metric: "Return Quarter-to-Date",
+      value: "-7.05%",
+    },
+    {
+      metric: "Return Year-to-Date",
+      value: "-7.05%",
+    },
+    {
+      metric: "Return Inception-to-Date",
+      value: "-7.05%",
+    },
+    {
+      metric: "Average Month",
+      value: "-7.05%",
+    },
+    {
+      metric: "Best Month",
+      value: "-7.05%",
+    },
+    {
+      metric: "Worst Month",
+      value: "-7.05%",
+    },
+    {
+      metric: "Positive Months",
+      value: "-7.05%",
+    },
+  ],}
+]
+
+export const riskMetricsData = [{
+  heading: "Risk Metrics",
+  info: [
+    {
+      metric: "Annualised Volatility",
+      value: "88.86%",
+      className: "w-[47%]",
+    },
+    {
+      metric: "Shape Ratio",
+      value: "0.47",
+      className: "w-[47%]",
+    },
+  ],
+}]
